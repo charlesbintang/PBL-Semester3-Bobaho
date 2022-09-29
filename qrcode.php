1cb2fba8090a93ed2e6cafe0af5b85776c89db89
jcsnewgame <johngaming909@gmail.com>
2022-09-29 16:42:31 +0700

    qrcode

--- qrcode.php
+++ qrcode.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -32,4 +33,40 @@
     </div>
     
 </body>
+=======
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Qrcode</title>
+    <link rel="stylesheet" href="css/qrcode.css?v=0.0.4">
+</head>
+<body>
+<header>
+        <img src="Judul.png" width="450px" id="Bobaandtea" alt="">
+        <img src="logo.png" width="250px" id="ayam" alt="">
+    </header>
+<h1>bayar disini</h1>
+<div class="eek">
+    <img src="Group 60.png" width="500px" alt="Taik">
+</div>
+
+<h2>atau melalui kasir</h2>
+
+<button type="button" class="buton">
+    Bayar Cash
+</button>
+
+<!-- tombol back -->
+<div>
+        <button class="tombolBack" type="button">
+            <span class="backButton">
+                <ion-icon name="arrow-round-back"></ion-icon>
+            </span>
+            <script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
+
+        </button>
+    </div>
+    
+</body>
+>>>>>>> 39a63c42ceff0ec61fe60709f15812ec6f4834fd
 </html>
