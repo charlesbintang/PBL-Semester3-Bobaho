39a63c42ceff0ec61fe60709f15812ec6f4834fd
jcsnewgame <113981005+jcsnewgame@users.noreply.github.com>
2022-09-29 11:17:32 +0700

    Add files via upload

---
+++ qrcode.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Qrcode</title>
+    <link rel="stylesheet" href="css/qrcode.css?v=0.0.4">
+</head>
+<body>
+<header>
+        <img src="Judul.png" width="450px" id="Bobaandtea" alt="">
+        <img src="logo.png" width="250px" id="ayam" alt="">
+    </header>
+<h1>bayar disini</h1>
+<div class="eek">
+    <img src="Group 60.png" width="500px" alt="Taik">
+</div>
+
+<h2>atau melalui kasir</h2>
+
+<button type="button" class="buton">
+    Bayar Cash
+</button>
+
+<!-- tombol back -->
+<div>
+        <button class="tombolBack" type="button">
+            <span class="backButton">
+                <ion-icon name="arrow-round-back"></ion-icon>
+            </span>
+            <script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
+
+        </button>
+    </div>
+    
+</body>
+</html>
