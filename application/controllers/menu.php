fcbebd8754afc37fa3f924b61d6e392a4c330bae
charlesbintang <charlesbintang203@gmail.com>
2022-12-14 13:47:27 +0700

    kirim notifikasi email berhasil

--- application/controllers/menu.php
+++ application/controllers/menu.php
@@ -43,11 +43,41 @@ class menu extends CI_Controller
     {
         error_reporting(0);
         $this->load->view('customer/notifQRIS');
+        $this->_sendEmail();
+    }
+
+    private function _sendEmail()
+    {
+        $config = [
+            'protocol' => 'smtp',
+            'smtp_host' => 'ssl://smtp.googlemail.com',
+            'smtp_user' => 'chadianjos3@gmail.com',
+            'smtp_pass' => 'osugmpspvxfrpxtz',
+            'smtp_port' => 465,
+            'mailtype' => 'html',
+            'charset' => 'utf-8',
+            'newline' => "\r\n"
+        ];
+
+        $this->email->initialize($config);
+
+        $this->email->from('chadianjos3@gmail.com', 'PBL-Semester3',);
+        $this->email->to('josephleonardo199@gmail.com');
+        $this->email->subject('Customer');
+        $this->email->message('Ada yang baru saja order nih, tolong dicek ya');
+
+        if ($this->email->send()) {
+            return true;
+        } else {
+            echo $this->email->print_debugger();
+            die;
+        }
     }
 
     public function cash()
     {
         error_reporting(0);
         $this->load->view('customer/notifCash');
+        $this->_sendEmail();
     }
 }
